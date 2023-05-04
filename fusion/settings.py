9a4190ffd0c4bdf2147dcc323d231912bf3e095f
AlanVitor <alanvictor100@hotmail.com>
2023-05-04 19:40:55 -0300

    fix media

--- fusion/settings.py
+++ fusion/settings.py
@@ -127,8 +127,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
 
 STATIC_ROOT = str(BASE_DIR / 'staticfiles')
 MEDIA_ROOT = str(BASE_DIR / 'media')
